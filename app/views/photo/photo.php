fcc6aa6dd4ad6593620f320adf1b4bc009802c04
vikavictory <54640442+vikavictory@users.noreply.github.com>
2020-08-10 22:56:50 +0300

    getUserMedia

--- app/views/photo/photo.php
+++ app/views/photo/photo.php
@@ -2,12 +2,21 @@
 <?php require_once "app/views/header.php" ?>
 <body>
 <?php require_once "app/views/navbar.php" ?>
+<link rel="stylesheet" type="text/css" href="/public/css/camera.css">
 
+<div class="item">
+    <span> video </span>
+    <video id="video" width="320" height="240" autoplay="autoplay" ></video>
+</div>
 
-<video id="video" width="640" height="480" autoplay></video>
-<button id="snap">Сделать снимок</button>
-<canvas id="canvas" width="640" height="480"></canvas>
+<div class="item">
+    <span> canvas </span>
+    <canvas id="canvas" width="320" height="240" ></canvas>
+</div>
 
+<input id="button" type="button" value="Snap" />
+
+<script src="/public/js/camera.js"></script>
 
 <form action="" method="post" enctype="multipart/form-data">
 	<input type="file" name="image">
--- public/js/camera.js
+++ public/js/camera.js
@@ -1,19 +1,29 @@
-if(navigator.webkitGetUserMedia!=null) {
-    var options = {
-        video:true,
-        audio:true
-    };
 
-    // запрашиваем доступ к веб-камере
-    navigator.webkitGetUserMedia(options,
-        function(stream) {
-            // получаем тег video
-            var video = document.querySelector('video');
-            // включаем поток в магический URL
-            video.src = window.webkitURL.createObjectURL(stream);
-        },
-        function(e) {
-            console.log("error happened ");
-        }
-    );
-}
+window.onload = function () {
+    var canvas = document.getElementById('canvas');
+    var video = document.getElementById('video');
+    var button = document.getElementById('button');
+    var context = canvas.getContext('2d')
+
+    var captureMe = function () {
+        context.translate(canvas.width, 0);
+        context.scale(-1, 1);
+        context.drawImage(video, 0, 0, video.width, video.height);
+        var base64dataUrl = canvas.toDataURL('image/png');
+        context.setTransform(1, 0, 0, 1, 0, 0);
+
+        // отправить на сервер в этом месте
+
+        var img = new Image();
+        img.src = base64dataUrl;
+        window.document.body.appendChild(img);
+    }
+
+    button.addEventListener('click', captureMe);
+
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(stream => {
+            video.srcObject = stream
+            video.play();
+        });
+};
